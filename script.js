6cb99783128c6b5d9591990a7a3d628734a04d21
Mihan-Coder <tahsinanzommihan@gmail.com>
2023-03-02 15:59:10 +0600

    Project Finished

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// login button handler
 var loginBtn = document.getElementById('login-btn')
 loginBtn.addEventListener("click", function(event){
     var loginArea = document.getElementById('login-area')
@@ -7,3 +8,50 @@ loginBtn.addEventListener("click", function(event){
     transactionArea.style.display = 'block'
 
 })
+
+// deposit button handler
+ var depositButton = document.getElementById('addDeposit')
+ depositButton.addEventListener("click", function(){
+    var depositNumber = getInputNumber('depositAmount')
+
+    updateSpanText('currentDeposit', depositNumber)
+    
+    updateSpanText('currentBalance', depositNumber)
+
+    document.getElementById('depositAmount').value = ''
+ })
+
+
+ function updateSpanText(id, depositNumber) {
+    var current = document.getElementById(id).innerText
+    var currentNumber = parseFloat(current)
+    var total = currentNumber + depositNumber
+ 
+    document.getElementById(id).innerText = total
+ }
+
+ function getInputNumber(id) {
+    var Amount = document.getElementById(id).value
+    var AmountNumber = parseFloat(Amount)
+    return AmountNumber
+    
+ }
+
+//  Money Withdraw handler
+
+var withdrawBtn = document.getElementById('addWithdraw')
+ 
+withdrawBtn.addEventListener('click', function(){
+    var withdrawNumber = getInputNumber("withdrawAmount")
+
+    updateSpanText("currentWithdraw", withdrawNumber)
+
+    updateSpanText('currentBalance', -1 * withdrawNumber)
+    
+    document.getElementById('withdrawAmount').value = ''
+
+})
+
+
+
+
